44c1dbf96c7da2d4b7961445a2d084cb02d3cd04
VincentiusF <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-05-02 15:11:00 +0700

    update

--- Laravelsi4b/Vincent1-app/routes/web.php
+++ Laravelsi4b/Vincent1-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,29 @@ route::get('about', function () {
 route::get('profil', function () {
     return view ('Profil');
 });
+
+// Route dengan parameter
+Route::get('Welcome/{salam}', function ($salam) {
+    // return 'Selamat '. $salam;
+    return view('salam')-> with('viewsalam', $salam);
+});
+
+// Route dengan parameter listdata
+// Terdapat array list
+Route::get('listdata', function () {
+    $list = ["Sistem Informasi", "Informatika", "Manajemen"];
+    $listmhs = [
+        ["npm" => "001", "nama" => "Ahmad"],
+        ["npm" => "002", "nama" => "Budi"]
+    ];
+    return view('listprodi')
+            ->with('viewlist', $list)
+            ->with('viewmhs', $listmhs);
+});
+
+Route::resource('fakultas', FakultasController::class);
+
+
+// Route::get('salam',function($salam) {
+// return 'Malam';
+// });
