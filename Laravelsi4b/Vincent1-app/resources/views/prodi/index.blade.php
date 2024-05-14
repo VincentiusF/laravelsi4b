94bb5c6c9f7333563ede9ec649244b747389963a
VincentiusF <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-05-14 17:00:20 +0700

    Database

--- Laravelsi4b/Vincent1-app/resources/views/prodi/index.blade.php
+++ Laravelsi4b/Vincent1-app/resources/views/prodi/index.blade.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fakultas</title>
+    <title>Program Studi</title>
 </head>
 <body>
     <h1>UMDP</h1>
-    <h2>Fakultas</h2>
+    <h2>Program Studi</h2>
     <ul>
-        @foreach ($fakultas as $item)
-            <li>{{ $item["nama"] }} {{ $item["singkatan"] }}</li>
+        @foreach ($prodi as $item)
+            <li>{{ $item["nama"] }} {{ $item["singkatam"] }} 
+            <br> {{ $item["fakultas"] ["nama"] }}</li>
         @endforeach
     </ul>
 </body>
--- Laravelsi4b/Vincent1-app/routes/web.php
+++ Laravelsi4b/Vincent1-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -36,6 +37,7 @@ Route::get('listdata', function () {
 });
 
 Route::resource('fakultas', FakultasController::class);
+Route::resource('prodi', ProdiController::class);
 
 
 // Route::get('salam',function($salam) {
