c0fa0ebe51ee60962ca1ba3d2e3d2967155ac3c5
VincentiusF <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2024-05-21 17:01:39 +0700

    template

--- Laravelsi4b/Vincent1-app/resources/views/prodi/index.blade.php
+++ Laravelsi4b/Vincent1-app/resources/views/prodi/index.blade.php
@@ -1,19 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Program Studi</title>
-</head>
-<body>
+@extends('layout.main')
+
+@section('title', 'Fakultas')
+    
+@section('content')
     <h1>UMDP</h1>
-    <h2>Program Studi</h2>
-    <ul>
-        @foreach ($prodi as $item)
-            <li>{{ $item["nama"] }} {{ $item["singkatam"] }} 
-            <br> {{ $item["fakultas"] ["nama"] }}</li>
-        @endforeach
-    </ul>
-</body>
-</html>
+    <div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Program Studi</h4>
+                  <p class="card-description">
+                  List data program studi
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Nama program studi</th>
+                          <th>Singkatan</th>
+                          <th>Nama Fakultas
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($prodi as $item)
+                            <tr>
+                                <td>{{ $item["nama"] }} 
+                                <td>{{ $item["singkatam"] }} </td>
+                                <td> {{ $item["fakultas"] ["nama"] }}</td>
+                            </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+</div>
+@endsection
